bff67a5145a7f3ced3a0656cd75173c86926541a
arsanious isaac <65775188+arsanious-isaac@users.noreply.github.com>
2023-04-13 22:33:37 +0200

    Update tasks.js

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,9 +3,15 @@ const {tasks} =  require('../models/tasks')
 const TaskSchema =  require('../models/tasks')
 const {validateTask} = require('../helpers/validation')    
 
-const getAllTasks = (req,res)=>{
-    res.send(tasks)
-}
+const getAllTasks = async (req,res)=>{
+    try{
+        const allTasks = await Task.find({}); //get all tasks
+        res.status(200).send(allTasks);
+
+    } catch(error) {
+        res.status(400).send(error);
+    }
+};
   
 module.exports={
     getAllTasks,
