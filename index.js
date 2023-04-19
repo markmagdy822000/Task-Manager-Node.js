98053b6433e8a4cfe43e4c15f63224f23749243b
MinaNagyLatif <66099836+MinaNagyLatif@users.noreply.github.com>
2023-04-19 13:39:09 +0200

    update

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // router is middle ware => a function injected when request comes
 // router send the request to suitable controller
+require("dotenv").config()
 const {app} = require('./app')
 
 const mongoose = require('mongoose')
 
-const port =  process.env.PORT || 3000
+const port =  process.env.PORT 
 app.listen(port,async ()=>{
-    const url="mongodb+srv://markmagdy:XqC7L3kstsTpnHfx@cluster0.gmcanta.mongodb.net/?retryWrites=true&w=majority"
+    const url=process.env.MONGODB_URI
     await mongoose.connect(url, {useNewUrlParser:true, useUnifiedTopology:true})
     console.log(`listening on https://localhost:${port}/api/v1/tasks`)
 })
