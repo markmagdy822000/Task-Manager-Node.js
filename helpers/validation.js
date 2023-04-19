4e6bb2303d087baf5b2afb1519d66fa536eee6e4
arsanious isaac <65775188+arsanious-isaac@users.noreply.github.com>
2023-04-19 11:29:37 +0200

    Update validation.js

--- helpers/validation.js
+++ helpers/validation.js
@@ -3,7 +3,9 @@ const Joi = require('joi')
 function validateTask(task){
     const schema = Joi.object({
         name:Joi.string().min(3).required(),
-        isCompleted:Joi.boolean()
+        isCompleted:Joi.boolean(),
+        archived:Joi.bool(),
+        dueDate:Joi.string()
     });
 
     return schema.validate(task)
